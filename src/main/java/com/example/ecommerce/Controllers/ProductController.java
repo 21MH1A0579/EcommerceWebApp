5ab0d6140111b9efa4c3ce85515a732d90b2ccb5
21MH1A0579 <21mh1a0579@acoe.edu.in>
2025-05-06 19:48:34 +0530

    Get Api Created

--- src/main/java/com/example/ecommerce/Controllers/ProductController.java
+++ src/main/java/com/example/ecommerce/Controllers/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import com.example.ecommerce.Services.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.AssertFalse.List;
 
 @Controller
 public class ProductController {
@@ -26,9 +28,19 @@ public class ProductController {
 			summary="Insert a new Product into DataBase."
 			
 			)
-	public ResponseEntity<Product> createProduct(@Valid @RequestBody Product newProduct){
-		return new ResponseEntity<Product>(newProduct, HttpStatus.CREATED);
+	public ResponseEntity<Product> createProduct(@org.springframework.web.bind.annotation.RequestBody Product newProduct){
+		Product insertedProduct=newService.insertOne(newProduct);
+		return new ResponseEntity<Product>(insertedProduct, HttpStatus.CREATED);
 		
+	}
+	
+	@GetMapping("/products")
+	@Operation(
+			summary="Get All Products From DataBase."
+			)
+	public ResponseEntity<Product> getAllProducts(){
+		java.util.List<Product> allProducts=newService.getAllProducts();
+		return new ResponseEntity<Product>(allProducts.get(0),HttpStatus.FOUND);
 	}
 
 }
--- src/main/java/com/example/ecommerce/Services/ProductService.java
+++ src/main/java/com/example/ecommerce/Services/ProductService.java
@@ -6,14 +6,21 @@ import org.springframework.stereotype.Service;
 import com.example.ecommerce.Models.Product;
 import com.example.ecommerce.Repository.ProductRepo;
 
+import jakarta.validation.constraints.AssertFalse.List;
+import java.util.*;
 @Service
 public class ProductService {
 	@Autowired
 	ProductRepo repo;
 	
 	public Product insertOne(Product newProd) {
-		
 		return repo.save(newProd);
+	}
+	
+	public java.util.List<Product> getAllProducts(){
+		
+		java.util.List<Product> productlist=repo.findAll();
+		return productlist;
 	}
 
 }
