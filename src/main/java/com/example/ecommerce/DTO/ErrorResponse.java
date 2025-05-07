68a2f92bad6cdee3afcb726b1fa2046d551ac643
21MH1A0579 <21mh1a0579@acoe.edu.in>
2025-05-07 15:05:38 +0530

    optimized the code using lambok dependency

--- src/main/java/com/example/ecommerce/DTO/ErrorResponse.java
+++ src/main/java/com/example/ecommerce/DTO/ErrorResponse.java
@@ -2,53 +2,30 @@ package com.example.ecommerce.DTO;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorResponse {
     private LocalDateTime timestamp;
     private int status;
     private String error;
     private String message;
     private String path;
-	public LocalDateTime getTimestamp() {
-		return timestamp;
-	}
-	public void setTimestamp(LocalDateTime timestamp) {
-		this.timestamp = timestamp;
-	}
-	public int getStatus() {
-		return status;
-	}
-	public void setStatus(int status) {
-		this.status = status;
-	}
-	public String getError() {
-		return error;
-	}
-	public void setError(String error) {
-		this.error = error;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public String getPath() {
-		return path;
-	}
-	public void setPath(String path) {
-		this.path = path;
-	}
 	public ErrorResponse(LocalDateTime timestamp, int status, String error, String message, String path) {
-		
+		super();
 		this.timestamp = timestamp;
 		this.status = status;
 		this.error = error;
 		this.message = message;
 		this.path = path;
 	}
-	public ErrorResponse() {
-		
-	}
+	
+	
 
     
 }
--- src/main/java/com/example/ecommerce/Models/Product.java
+++ src/main/java/com/example/ecommerce/Models/Product.java
@@ -14,9 +14,15 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 	     @Id
 	    @GeneratedValue(strategy = GenerationType.UUID)
@@ -52,85 +58,5 @@ public class Product {
 	        GROCERY
 	    }
 
-		public UUID getId() {
-			return id;
-		}
-
-		public void setId(UUID id) {
-			this.id = id;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public String getDescription() {
-			return description;
-		}
-
-		public void setDescription(String description) {
-			this.description = description;
-		}
-
-		public BigDecimal getPrice() {
-			return price;
-		}
-
-		public void setPrice(BigDecimal price) {
-			this.price = price;
-		}
-
-		public String getBrand() {
-			return brand;
-		}
-
-		public void setBrand(String brand) {
-			this.brand = brand;
-		}
-
-		public ProductCategory getCategory() {
-			return category;
-		}
-
-		public void setCategory(ProductCategory category) {
-			this.category = category;
-		}
-
-		public String getImageUrl() {
-			return imageUrl;
-		}
-
-		public void setImageUrl(String imageUrl) {
-			this.imageUrl = imageUrl;
-		}
-
-		public Product(UUID id, @NotBlank(message = "Product name is required") String name, String description,
-				@NotNull(message = "Price is required") @Positive(message = "Price must be positive") BigDecimal price,
-				String brand, ProductCategory category, String imageUrl) {
-			super();
-			this.id = id;
-			this.name = name;
-			this.description = description;
-			this.price = price;
-			this.brand = brand;
-			this.category = category;
-			this.imageUrl = imageUrl;
-		}
-
-		public Product() {
-			super();
-			// TODO Auto-generated constructor stub
-		}
-
-		@Override
-		public String toString() {
-			return "Product [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
-					+ ", brand=" + brand + ", category=" + category + ", imageUrl=" + imageUrl + "]";
-		}
-	    
-
+	   
 }
